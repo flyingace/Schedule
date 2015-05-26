d696925f2bd076a56b63259599eaf51e0cdc2e15
flyingace <dc@davidcameron.biz>
2015-05-25 23:57:12 -0400

    Modify Calendar.react.js and Schedule.js
    Played with Reactjs, trying to get top-level components to pass props to nested child components.
    
    Updated Schedule.js to output JSON of Months, Days and Shifts from date range. Intention is to pass JSON to Mongodb.

--- build/Calendar.react.js
+++ build/Calendar.react.js
@@ -23,3 +23,20 @@ React.render(
     React.createElement(Calendar, {monthName: "August", dayName: "Monday", shiftName: "LDPM"}),
     document.getElementById('calendar_wrapper')
 );
+
+/*
+Start with a date range
+Loop through each date
+Date object contains Day, Month, Year, Day of Week
+For each Month create a Month element
+Render each Month
+For each day in a month create a Day element
+Extract Date and Day of Week from Date Object
+Render each Day, Display Day of Week and Day of Month
+For each Day of Week Determine Shifts
+Render each shift
+
+!!!
+Start with Date Range
+Create JSON object for All Months, Days, Shifts
+ */
--- build/Schedule.js
+++ build/Schedule.js
@@ -3,186 +3,347 @@
 /*globals moment */
 
 var Schedule = (function () {
-    var shift,
-        staff = [],
-        assignedShifts = [],
-        _calendar = [],
-        _createEmployee,
-        _determineCalendar,
-        _addEmployeeToStaff,
-        _createShift,
-        _createShifts,
-        _createStaff,
-        _assignShift,
-        _assignShifts,
-        _getShifts,
-        _getCoverage,
-        _chooseRandomEmployee,
-        _adjustCandidateAvailability,
-        _checkAvailability;
+            var shift,
+                staff = [],
+                assignedShifts = [],
+                _calendar = [],
+                _createEmployee,
+                _determineCalendar,
+                _addEmployeeToStaff,
+                _createShift,
+                _createShifts,
+                _createStaff,
+                _assignShift,
+                _assignShifts,
+                _getShifts,
+                _getCoverage,
+                _chooseRandomEmployee,
+                _adjustCandidateAvailability,
+                _checkAvailability;
 
-    _createEmployee = function _createEmployee(empName, contractHours) {
-        var employee = {
-            name: empName,
-            contractHours: contractHours,
-            unassignedHours: contractHours
-        };
+            _createEmployee = function _createEmployee(empName, contractHours) {
+                var employee = {
+                    name: empName,
+                    contractHours: contractHours,
+                    unassignedHours: contractHours
+                };
 
-        return employee;
-    };
+                return employee;
+            };
 
-    _createStaff = function _createStaff(empJSON) {
-        for (var i = 0; i < empJSON.length; i++) {
-            _addEmployeeToStaff(_createEmployee(empJSON[i].name, empJSON[i].hours))
+            _createStaff = function _createStaff(empJSON) {
+                for (var i = 0; i < empJSON.length; i++) {
+                    _addEmployeeToStaff(_createEmployee(empJSON[i].name, empJSON[i].hours))
+                }
+
+                console.log(staff);
+            };
+
+            _addEmployeeToStaff = function _addEmployeeToStaff(employee) {
+                staff.push(employee);
+
+            };
+
+            _createShifts = function _createShifts(dayInfo) {
+                var shiftNameArray = dayInfo.shifts,
+                    shiftDate = dayInfo.date,
+                    dayOfShifts = [];
+
+                for (var i = 0; i < shiftNameArray.length; i++) {
+                    var newShift = _createShift(shiftNameArray[i], shiftDate);
+                    dayOfShifts.push(newShift);
+                }
+
+                return dayOfShifts;
+            };
+
+            _createShift = function _createShift(shiftName, shiftDate) {
+                var shift = {};
+
+                shift.name = shiftName;
+                shift.date = shiftDate;
+                shift.length = (shiftName.indexOf('L&D') < 0) ? 8 : 12;
+                shift.assignee = '';
+
+                return shift;
+            };
+
+            /**
+             * Create array of consecutive dates starting with startDate and
+             * ending with endDate in the format 'dddd, MMMM D YYYY' (eg, 'Sunday, May 10, 2015')
+             * startDate & endDate are passed as 'YYYY-MM-DD' (eg, '2015-05-23')
+             * @param startDate
+             * @param endDate
+             * @returns {Array}
+             * @private
+             */
+            _determineCalendar = function _determineCalendar(startDate, endDate) {
+
+                var firstDate = moment(startDate),
+                    lastDate = moment(endDate),
+                    dateInRange = firstDate,
+                    calendarJSON = {},
+                    formattedDate,
+                    currentMonth = '',
+                    dayObj,
+                    dayDate;
+
+                //check if the date comes after the lastDate, so lastDate is included in the array
+                while (!dateInRange.isAfter(lastDate)) {
+                    //_calendar.push(dateInRange.format('dddd, MMMM D YYYY'));
+                    //dateInRange.add(1, 'days');
+                    formattedDate = dateInRange.format('dddd, MMMM, D, YYYY').split(', ');
+
+                    dayDate = parseInt(formattedDate[2], 10);
+                    dayObj = _getDayObject(formattedDate[0], dayDate);
+
+                    if (formattedDate[1] !== currentMonth) {
+                        currentMonth = formattedDate[1];
+                        calendarJSON[currentMonth] = {"Days": []};
+                    }
+                    calendarJSON[currentMonth].Days.push(dayObj);
+
+                    dateInRange.add(1, 'days');
+                }
+
+
+                /*
+                 Is the month the same as the month of the current month array object
+                 No: Start new Month
+                 Yes: Create a new day with date and Day of week params
+                 Add appropriate shifts based on Day of Week
+                 */
+
+                /*
+                 calendarJSON = {
+                 "May": {
+                 "MonthName": "STRING",
+                 "Days": [
+                 {
+                 "DayDate": "INTEGER",
+                 "DayName": "STRING",
+                 "LD_Day": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "LD_Night": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "Clinic1": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "Clinic2": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "Clinic3": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 }
+                 },
+                 {
+                 "DayDate": "INTEGER",
+                 "DayName": "STRING",
+                 "LD_Day": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "LD_Night": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "Clinic1": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "Clinic2": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "Clinic3": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 }
+                 },
+                 {
+                 "DayDate": "INTEGER",
+                 "DayName": "STRING",
+                 "LD_Day": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "LD_Night": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "Clinic1": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "Clinic2": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 },
+                 "Clinic3": {
+                 "required": "BOOLEAN",
+                 "assignee": ""
+                 }
+                 }
+                 ]
+                 }
+                 };
+                 */
+
+                console.log(JSON.stringify(calendarJSON));
+            };
+
+            _getDayObject = function _getDayObject(dayName, dayDate) {
+                var shiftRequirementArray = [];
+
+                switch (dayName) {
+                    case 'Monday':
+                    case 'Thursday':
+                        shiftRequirementArray = [true, true, true, true, true, true];
+                        break;
+                    case 'Tuesday':
+                    case 'Wednesday':
+                    case 'Friday':
+                        shiftRequirementArray = [true, true, true, true, true, false];
+                        break;
+                    case 'Saturday':
+                    case 'Sunday':
+                        shiftRequirementArray = [true, true, false, false, false, false];
+                        break;
+                    default:
+                        break;
+                }
+
+                return {
+                    "DayDate": dayDate,
+                    "DayName": dayName,
+                    "LD_Day": {
+                        "required": shiftRequirementArray[0],
+                        "assignee": ""
+                    },
+                    "LD_Night": {
+                        "required": shiftRequirementArray[1],
+                        "assignee": ""
+                    },
+                    "Clinic1": {
+                        "required": shiftRequirementArray[2],
+                        "assignee": ""
+                    },
+                    "Clinic2": {
+                        "required": shiftRequirementArray[3],
+                        "assignee": ""
+                    },
+                    "Clinic3": {
+                        "required": shiftRequirementArray[4],
+                        "assignee": ""
+                    },
+                    "Clinic3": {
+                        "required": shiftRequirementArray[5],
+                        "assignee": ""
+                    }
+                }
+            };
+
+
+            _getShifts = function _getShifts(day) {
+                var dayAndDate = day.split(', '),
+                    shiftsForDay = {date: dayAndDate[1]};
+
+                switch (dayAndDate[0]) {
+                    case 'Monday':
+                    case 'Thursday':
+                        shiftsForDay.shifts = ["L&D_Day", "L&D_Night", "Clinic", "Hi_Risk"];
+                        break;
+                    case 'Tuesday':
+                    case 'Wednesday':
+                    case 'Friday':
+                        shiftsForDay.shifts = ["L&D_Day", "L&D_Night", "Clinic"];
+                        break;
+                    case 'Saturday':
+                    case 'Sunday':
+                        shiftsForDay.shifts = ["L&D_Day", "L&D_Night"];
+                        break;
+                    default:
+                        break;
+                }
+
+                return shiftsForDay;
+            };
+
+            _assignShifts = function _assignShifts(shiftsForDay) {
+
+                for (var i = 0; i < shiftsForDay.length; i++) {
+                    _assignShift(shiftsForDay[i]);
+                }
+            };
+
+            _assignShift = function _assignShift(shift) {
+                var candidate;
+
+                while (shift.assignee === '') {
+                    candidate = _chooseRandomEmployee();
+                    var candidateIsAvailable = _checkAvailability(candidate, shift.length);
+
+                    if (candidateIsAvailable) {
+                        _adjustCandidateAvailability(candidate, shift.length);
+                        shift.assignee = candidate.name;
+                    }
+                }
+            };
+
+//TODO: Maybe add something to this that checks whether the selected employee has
+//a ratio of unassignedHours/contractHours that is above or below the average ratio
+//of all other employees
+            _chooseRandomEmployee = function _chooseRandomEmployee() {
+                var empIndex = Math.floor(Math.random() * staff.length);
+
+                return staff[empIndex];
+            };
+
+            _checkAvailability = function _checkAvailability(candidate, shiftLength) {
+                return (candidate.unassignedHours >= shiftLength);
+            };
+
+            _adjustCandidateAvailability = function _adjustCandidateAvailability(candidate, shiftLength) {
+                candidate.unassignedHours -= shiftLength;
+            };
+
+            _getCoverage = function _getCoverage() {
+                var calendar = _calendar,
+                    dayInformation,
+                    dayOfShifts = [];
+
+                for (var i = 0; i < calendar.length; i++) {
+                    dayInformation = _getShifts(calendar[i]);
+                    dayOfShifts = _createShifts(dayInformation);
+                    _assignShifts(dayOfShifts);
+                    assignedShifts.push(dayOfShifts);
+                }
+
+                console.log(JSON.stringify(assignedShifts));
+
+            };
+
+            return {
+                //createEmployee: _createEmployee,
+                //addEmployeeToStaff: _addEmployeeToStaff,
+                //createShift: _createShift,
+                //addShiftToSchedule: _addShiftToSchedule,
+                createStaff: _createStaff,
+                determineCalendar: _determineCalendar,
+                getCoverage: _getCoverage
+            };
+
         }
-
-        console.log(staff);
-    };
-
-    _addEmployeeToStaff = function _addEmployeeToStaff(employee) {
-        staff.push(employee);
-
-    };
-
-    _createShifts = function _createShifts(dayInfo) {
-        var shiftNameArray = dayInfo.shifts,
-            shiftDate = dayInfo.date,
-            dayOfShifts = [];
-
-        for (var i = 0; i < shiftNameArray.length; i++) {
-            var newShift = _createShift(shiftNameArray[i], shiftDate);
-            dayOfShifts.push(newShift);
-        }
-
-        return dayOfShifts;
-    };
-
-    _createShift = function _createShift(shiftName, shiftDate) {
-        var shift = {};
-
-        shift.name = shiftName;
-        shift.date = shiftDate;
-        shift.length = (shiftName.indexOf('L&D') < 0) ? 8 : 12;
-        shift.assignee = '';
-
-        return shift;
-    };
-
-    /**
-     * Create array of consecutive dates starting with startDate and
-     * ending with endDate in the format 'dddd, MMMM D YYYY' (eg, 'Sunday, May 10, 2015')
-     * startDate & endDate are passed as 'YYYY-MM-DD' (eg, '2015-05-23')
-     * @param startDate
-     * @param endDate
-     * @returns {Array}
-     * @private
-     */
-    _determineCalendar = function _determineCalendar(startDate, endDate) {
-
-        var firstDate = moment(startDate),
-            lastDate = moment(endDate),
-            dateInRange = firstDate;
-
-        //check if the date comes after the lastDate, so lastDate is included in the array
-        while (!dateInRange.isAfter(lastDate)) {
-            _calendar.push(dateInRange.format('dddd, MMMM D YYYY'));
-            dateInRange.add(1, 'days');
-        }
-
-        console.log(_calendar);
-    };
-
-
-    _getShifts = function _getShifts(day) {
-        var dayAndDate = day.split(', '),
-            shiftsForDay = {date: dayAndDate[1]};
-
-        switch (dayAndDate[0]) {
-            case 'Monday':
-            case 'Thursday':
-                shiftsForDay.shifts = ["L&D_Day", "L&D_Night", "Clinic", "Hi_Risk"];
-                break;
-            case 'Tuesday':
-            case 'Wednesday':
-            case 'Friday':
-                shiftsForDay.shifts = ["L&D_Day", "L&D_Night", "Clinic"];
-                break;
-            case 'Saturday':
-            case 'Sunday':
-                shiftsForDay.shifts = ["L&D_Day", "L&D_Night"];
-                break;
-            default:
-                break;
-        }
-
-        return shiftsForDay;
-    };
-
-    _assignShifts = function _assignShifts(shiftsForDay) {
-
-        for (var i = 0; i < shiftsForDay.length; i++) {
-            _assignShift(shiftsForDay[i]);
-        }
-    };
-
-    _assignShift = function _assignShift(shift) {
-        var candidate;
-
-        while (shift.assignee === '') {
-            candidate = _chooseRandomEmployee();
-            var candidateIsAvailable = _checkAvailability(candidate, shift.length);
-
-            if (candidateIsAvailable) {
-                _adjustCandidateAvailability(candidate, shift.length);
-                shift.assignee = candidate.name;
-            }
-        }
-    };
-
-    //TODO: Maybe add something to this that checks whether the selected employee has
-    //a ratio of unassignedHours/contractHours that is above or below the average ratio
-    //of all other employees
-    _chooseRandomEmployee = function _chooseRandomEmployee() {
-        var empIndex = Math.floor(Math.random() * staff.length);
-
-        return staff[empIndex];
-    };
-
-    _checkAvailability = function _checkAvailability(candidate, shiftLength) {
-        return (candidate.unassignedHours >= shiftLength);
-    };
-
-    _adjustCandidateAvailability = function _adjustCandidateAvailability(candidate, shiftLength) {
-        candidate.unassignedHours -= shiftLength;
-    };
-
-    _getCoverage = function _getCoverage() {
-        var calendar = _calendar,
-            dayInformation,
-            dayOfShifts = [];
-
-        for (var i = 0; i < calendar.length; i++) {
-            dayInformation = _getShifts(calendar[i]);
-            dayOfShifts = _createShifts(dayInformation);
-            _assignShifts(dayOfShifts);
-            assignedShifts.push(dayOfShifts);
-        }
-
-        console.log(JSON.stringify(assignedShifts));
-
-    };
-
-    return {
-        //createEmployee: _createEmployee,
-        //addEmployeeToStaff: _addEmployeeToStaff,
-        //createShift: _createShift,
-        //addShiftToSchedule: _addShiftToSchedule,
-        createStaff: _createStaff,
-        determineCalendar: _determineCalendar,
-        getCoverage: _getCoverage
-    };
-
-}());
+        ()
+    )
+    ;
 
 var employees = {
     employees: [
@@ -251,6 +412,6 @@ var employees = {
 
 
 Schedule.createStaff(employees.employees);
-Schedule.determineCalendar('05-03-2015', '05-08-2015');
+Schedule.determineCalendar('05-01-2015', '07-31-2015');
 //Schedule.getCoverage();
 
